27c71774adbb6151f1e8bde1d210998ecb05af38
anastasiia <anastasiya1155@gmail.com>
2023-12-10 09:50:35 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anastasiy1155/profit-calculator
+
+go 1.21.5
---
+++ profit_calculator.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue, expenses float64
+	var tax_rate int
+	fmt.Print("Input your revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Input your expenses amount: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Input your tax rate (%): ")
+	fmt.Scan(&tax_rate)
+
+	var ebt = revenue - expenses
+	var profit = ebt * (1 - float64(tax_rate)/100)
+	var ratio = ebt / profit
+
+	fmt.Println("Your earning before tax ", ebt, ", profit ", profit, ", ratio ", ratio)
+}
